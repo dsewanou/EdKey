5a847d8d682c89a6e7a53c21bf1da6c31e8f5c4e
unknown <bignonboris@gmail.com>
2026-02-19 08:46:12 +0100

    intégration de la procedure multi-tenancy

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider; 
+use Illuminate\Support\Facades\Event; // Importe la façade Event
+use Stancl\Tenancy\Events\TenantCreated; // L'événement du package
+use App\Listeners\CreateTenantAdmin; // Ton futur 
+use Stancl\Tenancy\Events\DatabaseCreated;
+use Stancl\Tenancy\Events\TenantHasMigrated; // Import correct
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // On lie l'événement de création du tenant à notre Listener
+        Event::listen(
+            // TenantCreated::class,
+            // DatabaseCreated::class, // On attend que la DB soit réellement là
+            TenantHasMigrated::class,
+            CreateTenantAdmin::class,
+        );
     }
 }
